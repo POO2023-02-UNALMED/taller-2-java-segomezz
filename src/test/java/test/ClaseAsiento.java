e6411b1084b9fc1e34dab257df6718b16b860d59
segomezz <141938389+segomezz@users.noreply.github.com>
2023-08-20 08:15:04 -0500

    Clase Asiento

---
+++ src/test/java/test/ClaseAsiento.java
@@ -0,0 +1,11 @@
+package test;
+
+public class ClaseAsiento {
+	public static void main(String[] args) {
+		String color;
+		int precio;
+		int registro;
+		public final static cambiarColor(String rojo, String verde,String amarillo,String negro,String blanco);
+	}
+
+}
